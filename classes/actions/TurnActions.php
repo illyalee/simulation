34c7e56f8395d9f469cef3ab27ff53f1a0d4107a
Илья Ли <ilali@Ilas-MacBook-Air.local>
2024-10-01 09:15:29 +0300

    gg

--- classes/actions/TurnActions.php
+++ classes/actions/TurnActions.php
@@ -15,12 +15,13 @@ class TurnActions
 
     public function movePieceManualy(Map $map)
     {
+        //manual mova Object from one cell to another
         $start = $this->getUserCoords();
         $end = $this->getUserCoords();
         $mapArr = $map->getMap(); // copy of map array
         $element = $mapArr[$start[0]][$start[1]];
         $mapArr[$start[0]][$start[1]] = null;
         $mapArr[$end[0]][$end[1]] = $element;
-        $map->setMap($mapArr);// replace old map with new map
+        $map->setMap($mapArr); // replace old map with new map
     }
 }
